64ffacc4568010ba137e4287a9a56825547ee535
Михаил Постников <mihha2397@mail.ru>
2022-10-20 21:08:47 +0300

    render v1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+from re import template
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,8 +6,9 @@ from django.http import HttpResponse
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    template='posts/index.html'    
+    return render(request, template) 
 
 def group_posts(request,slug):
     return HttpResponse('gryppa')
