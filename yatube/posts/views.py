f190d681fe7fcb0e3637adffa8f3f7bf9c3389da
Михаил Постников <mihha2397@mail.ru>
2022-10-25 22:54:25 +0300

    baza

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,21 @@
 from re import template
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 from django.http import HttpResponse
 
 
 # Главная страница
 def index(request):
-    template='posts/index.html'
-    text="Это главная страница проекта Yatube"
-    context={
-        'text':text
-    }        
-    return render(request, template, context) 
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 def group_posts(request,slug):
     template='posts/group_list.html'
